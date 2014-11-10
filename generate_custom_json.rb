d4dc9478d5c1e5a5bab276025a47d7ae3e4fc849
jlhonora <jlhonora@ing.puc.cl>
2014-11-10 19:01:52 -0300

    Small modifications

--- generate_custom_json.rb
+++ generate_custom_json.rb
@@ -8,9 +8,14 @@ app_folder  = "../beetrack"
 environment = "production"
 files       = ["config/database.yml", "config/mongoid.yml", "config/services_config.yml"]
 
+result = Hash.new
 files.each do |f|
   loaded = YAML.load_file("#{app_folder}/#{f}")
   puts "#{f}"
+  parent = f.split("/").last.split(".").first
+  result[parent] = loaded[environment]
   puts loaded[environment].to_json
   puts ""
 end
+
+puts JSON.pretty_generate(result)
