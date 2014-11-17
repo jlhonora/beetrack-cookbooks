c8679c3e6efb0e6263fde889bd06c80280f891bb
jlhonora <jlhonora@ing.puc.cl>
2014-11-17 12:26:54 -0300

    Correct path for templates

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,10 +18,10 @@ beetrack_templates = ["database", "mongoid", "services_config"]
 
 beetrack_templates.each do |t|
   puts "Inflating template #{t}.yml.erb"
-  template "/srv/beetrack/shared/config/#{t}.yml" do
+  template "/srv/www/beetrack/shared/config/#{t}.yml" do
     source "#{t}.yml.erb"
-    owner  "ubuntu"
-    group  "ubuntu"
-    mode   "0660"
+    owner  "deploy"
+    group  "www-data"
+    mode   "0664"
   end
 end
