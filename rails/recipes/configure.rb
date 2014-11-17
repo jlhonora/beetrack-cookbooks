27111d9126d7703a376d5391fa1ca85d8f7b1dde
jlhonora <jlhonora@ing.puc.cl>
2014-11-17 12:45:05 -0300

    Modifying OpsWorks default structure

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -19,7 +19,20 @@ package "libxml2-dev"
 beetrack_templates = ["database", "mongoid", "services_config"]
 
 node[:deploy].each do |application, deploy|
+  node.default[:deploy][application][:database][:adapter] = 
+    OpsWorks::RailsConfiguration.determine_database_adapter(
+      application, 
+      node[:deploy][application],
+      "#{node[:deploy][application][:deploy_to]}/current",
+      :force => node[:force_database_adapter_detection])
+  
   deploy = node[:deploy][application]
+
+  execute "restart Rails app #{application}" do
+    cwd deploy[:current_path]
+    command node[:opsworks][:rails_stack][:restart_command]
+    action :nothing
+  end
 
   beetrack_templates.each do |t|
     puts "Inflating template #{t}.yml.erb"
