3ba2488fc95422159c8af5ec91d8005d584f08d7
Antonio Grass <antonio_grass@hotmail.com>
2015-01-08 16:08:14 -0300

    change append line

--- beetrack_security/recipes/nginx.rb
+++ beetrack_security/recipes/nginx.rb
@@ -13,14 +13,14 @@ node[:deploy].each do |application, deploy|
     block do
       Chef::Log.info("creating config nginx beetrack in #{rails_env}")
       file = Chef::Util::FileEdit.new("/etc/nginx/sites-available/beetrack")
-      file.search_file_delete_line("/client_max_body_size 10m;/")
-      file.search_file_delete_line("/client_body_buffer_size 128k;/")
-      file.search_file_delete_line("/limit_req   zone=one  burst=5 nodelay;/")
-      file.search_file_delete_line("/proxy_set_header X-Real-IP $remote_addr;/")
-      file.insert_line_after_match("/location @unicorn {/", "proxy_set_header X-Real-IP $remote_addr;")
-      file.insert_line_after_match("/location @unicorn {/", "client_max_body_size 10m;")
-      file.insert_line_after_match("/location @unicorn {/", "client_body_buffer_size 128k;")
-      file.insert_line_after_match("/location @unicorn {/", "limit_req   zone=one  burst=5 nodelay;")
+      #file.search_file_delete_line("/client_max_body_size 10m/")
+      #file.search_file_delete_line("/client_body_buffer_size 128k/")
+      #file.search_file_delete_line("/limit_req   zone=one  burst=5 nodelay/")
+      #file.search_file_delete_line("/proxy_set_header X-Real-IP $remote_addr;/")
+      file.insert_line_after_match("/location @unicorn/", "proxy_set_header X-Real-IP $remote_addr;")
+      file.insert_line_after_match("/location @unicorn/", "client_max_body_size 10m;")
+      file.insert_line_after_match("/location @unicorn/", "client_body_buffer_size 128k;")
+      file.insert_line_after_match("/location @unicorn/", "limit_req   zone=one  burst=5 nodelay;")
       file.write_file
     end
   end
