5c0d088f55116aa668a8b3a3e0281e8eb6915bbd
Antonio Grass <antonio_grass@hotmail.com>
2015-02-11 15:28:23 -0300

    change recipe set key

--- opsworks-elb/recipes/register.rb
+++ opsworks-elb/recipes/register.rb
@@ -1,6 +1,6 @@
 include_recipe "opsworks-elb"
 
 execute "register" do
-  command "aws elb register-instances-with-load-balancer --load-balancer-name #{node[:aws][:elb][:load_balancer_name]} --instances #{node[:opsworks][:instance][:aws_instance_id]} --region #{node[:opsworks][:instance][:region]} --access-key-id #{node['aws']['AWS_ACCESS_KEY_ID']} --secret-key #{node['aws']['AWS_SECRET_ACCESS_KEY']}"
+  command "aws elb register-instances-with-load-balancer --load-balancer-name #{node[:aws][:elb][:load_balancer_name]} --instances #{node[:opsworks][:instance][:aws_instance_id]} --region #{node[:opsworks][:instance][:region]}"
   user "root"
 end
