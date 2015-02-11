d49397917ec070ce3ad81794dc34192cb6aed672
Antonio Grass <antonio_grass@hotmail.com>
2015-02-11 17:01:37 -0300

    add recipe keys

--- opsworks-elb/recipes/default.rb
+++ opsworks-elb/recipes/default.rb
@@ -1,14 +1,4 @@
 ## cookbooks/aws/recipes/default.rb
-execute "aws-key" do
-  command "export AWS_ACCESS_KEY_ID=#{node['aws']['AWS_ACCESS_KEY_ID']}"
-  action :run
-end
-
-execute "aws-pwd" do
-  command "export AWS_SECRET_ACCESS_KEY=#{node['aws']['AWS_SECRET_ACCESS_KEY']}"
-  action :run
-end
-
 execute "aws-cli" do
   command "sudo apt-get -y install awscli"
   user "root"
