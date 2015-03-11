a5cd4cc0b6e7017978de419b07394fbe64ed175a
Antonio Grass <antonio_grass@hotmail.com>
2015-03-11 13:37:14 -0300

    add start sidekiq option

--- beetrack_rails/recipes/start_sidekiq.rb
+++ beetrack_rails/recipes/start_sidekiq.rb
@@ -4,8 +4,7 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("shutdown sidekiq #{rails_env}")
   execute "rake sidekiq:start" do
     user 'root'
-    command       "bundle exec rake sidekiq:start"
+    command       "bundle exec rake sidekiq:start RAILS_ENV=#{rails_env}"
     cwd           release_path
-    environment   'RAILS_ENV' => rails_env
   end
 end
