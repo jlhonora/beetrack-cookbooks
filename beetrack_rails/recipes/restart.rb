a784f3e75673b7ef48d624ac22b20b9160042653
Antonio Grass <antonio_grass@hotmail.com>
2015-03-11 11:42:36 -0300

    deploy user change

--- beetrack_rails/recipes/restart.rb
+++ beetrack_rails/recipes/restart.rb
@@ -2,11 +2,14 @@ node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   release_path = "/srv/www/beetrack/current/"
   execute "unicorn_#{application} restart" do
+    user 'deploy'
+    group 'deploy'
     command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
     action :run
   end
   execute "rake sidekiq:start" do
     command       "bundle exec rake sidekiq:start"
     cwd           release_path
+    action :run
   end
 end
