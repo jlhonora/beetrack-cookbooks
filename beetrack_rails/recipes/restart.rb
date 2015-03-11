d202c56cc1c9aa669ddd0aa1f0098e063d67134b
Antonio Grass <antonio_grass@hotmail.com>
2015-03-11 10:51:02 -0300

    hotfix

--- beetrack_rails/recipes/restart.rb
+++ beetrack_rails/recipes/restart.rb
@@ -1,4 +1,6 @@
 node[:deploy].each do |application, deploy|
+  rails_env = deploy[:rails_env]
+  release_path = "/srv/www/beetrack/current/"
   execute "unicorn_#{application} restart" do
     command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
     action :run
