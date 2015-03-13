a07b468b03879786d44fabc60d7d224d7701cfbd
Antonio Grass <antonio_grass@hotmail.com>
2015-03-13 18:56:09 -0300

    add sidekiq stop

--- beetrack_rails/recipes/start_sidekiq.rb
+++ beetrack_rails/recipes/start_sidekiq.rb
@@ -2,9 +2,12 @@ node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   release_path = "/srv/www/beetrack/current/"
   Chef::Log.info("shutdown sidekiq #{rails_env}")
-  execute "rake sidekiq:start" do
-    user 'root'
-    command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d"
-    cwd           release_path
+  SIDEKIQ_PID = File.expand_path("/srv/www/beetrack/shared/pids/sidekiq.pid", __FILE__)
+  if !( File.exists?(SIDEKIQ_PID) && system("ps x | grep `cat #{SIDEKIQ_PID}` 2>&1 > /dev/null") rescue false)
+    execute "rake sidekiq:start" do
+      user 'root'
+      command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d"
+      cwd           release_path
+    end
   end
 end
