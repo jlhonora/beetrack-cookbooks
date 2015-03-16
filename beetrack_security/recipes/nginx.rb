381262546ae090d116e251302ff68efdfd18af01
Antonio Grass <antonio_grass@hotmail.com>
2015-03-16 17:07:35 -0300

    fixing bug template

--- beetrack_security/recipes/nginx.rb
+++ beetrack_security/recipes/nginx.rb
@@ -1,14 +1,16 @@
-template '/etc/nginx/nginx.conf' do
-  source 'nginx.conf.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-end
+node[:deploy].each do |application, deploy|
+  template '/etc/nginx/nginx.conf' do
+    source 'nginx.conf.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
 
-template '/etc/nginx/sites-available/beetrack' do
-  source 'beetrack.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
-end
+  template '/etc/nginx/sites-available/beetrack' do
+    source 'beetrack.erb'
+    owner 'root'
+    group 'root'
+    mode 0644
+  end
 
+end
