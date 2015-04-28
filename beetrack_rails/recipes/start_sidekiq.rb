0f412de024eafda1a4dc9245b6a013ae17448de6
Antonio Grass <antonio_grass@hotmail.com>
2015-04-28 10:38:54 -0300

    fixing deploy sidekiq

--- beetrack_rails/recipes/start_sidekiq.rb
+++ beetrack_rails/recipes/start_sidekiq.rb
@@ -1,20 +1,16 @@
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   release_path = "#{deploy[:deploy_to]}/current/"
-  Chef::Log.info("shutdown sidekiq #{rails_env}")
-  SIDEKIQ_PID = File.expand_path("#{deploy[:deploy_to]}/shared/pids/sidekiq.pid", __FILE__)
-  if !( File.exists?(SIDEKIQ_PID) && system("ps x | grep `cat #{SIDEKIQ_PID}` 2>&1 > /dev/null") rescue false)
-    execute "rake sidekiq:start" do
-      user 'root'
-      command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d"
-      cwd           release_path
-    end
-  else
-    execute "rake sidekiq:stop" do
-      cwd           release_path
-      user 'root'
-      command "RAILS_ENV=#{rails_env} bundle exec sidekiqctl stop #{deploy[:deploy_to]}/shared/pids/sidekiq.pid"
-      returns [0,1]
-    end
+  execute "rake sidekiq:stop" do
+    cwd           release_path
+    user 'root'
+    command "RAILS_ENV=#{rails_env} bundle exec sidekiqctl stop #{deploy[:deploy_to]}/shared/pids/sidekiq.pid"
+    returns [0,1]
+  end
+
+  execute "rake sidekiq:start" do
+    user 'root'
+    command       "RAILS_ENV=#{rails_env} bundle exec sidekiq -d"
+    cwd           release_path
   end
 end
