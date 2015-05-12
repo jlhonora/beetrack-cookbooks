6f2794e70e299fc788b25c3ec15756a24fcde520
Antonio Grass <antonio_grass@hotmail.com>
2015-05-12 14:01:58 -0300

    add create swap recipe

--- beetrack_rails/recipes/create_swap.rb
+++ beetrack_rails/recipes/create_swap.rb
@@ -1,20 +1,23 @@
-script 'create swapfile' do
-  interpreter 'bash'
-  not_if { File.exists?('/var/swapfile') }
-  code <<-eof
-    dd if=/dev/zero of=/var/swapfile bs=1M count=8192 &&
+node[:deploy].each do |application, deploy|
+  swap_size = node['swap_size'] || 500
+  script 'create swapfile' do
+    interpreter 'bash'
+    not_if { File.exists?('/var/swapfile') }
+    code <<-eof
+    dd if=/dev/zero of=/var/swapfile bs=1M count=#{swap_size} &&
     chmod 600 /var/swapfile &&
     mkswap /var/swapfile
-  eof
-end
+    eof
+  end
 
-mount '/dev/null' do  # swap file entry for fstab
-  action :enable  # cannot mount; only add to fstab
-  device '/var/swapfile'
-  fstype 'swap'
-end
+  mount '/dev/null' do  # swap file entry for fstab
+    action :enable  # cannot mount; only add to fstab
+    device '/var/swapfile'
+    fstype 'swap'
+  end
 
-script 'activate swap' do
-  interpreter 'bash'
-  code 'swapon -a'
+  script 'activate swap' do
+    interpreter 'bash'
+    code 'swapon -a'
+  end
 end
