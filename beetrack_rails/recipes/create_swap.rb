f61f6fc22aca5483d73be6eec230b9a3eebedf75
Antonio Grass <antonio_grass@hotmail.com>
2015-05-12 16:05:31 -0300

    update swap recipe

--- beetrack_rails/recipes/create_swap.rb
+++ beetrack_rails/recipes/create_swap.rb
@@ -2,17 +2,17 @@ node[:deploy].each do |application, deploy|
   swap_size = node['swap_size'] || 500
   script 'create swapfile' do
     interpreter 'bash'
-    not_if { File.exists?('/var/swapfile') }
+    not_if { File.exists?('/mnt/swap') }
     code <<-eof
-    dd if=/dev/zero of=/var/swapfile bs=1M count=#{swap_size} &&
-    chmod 600 /var/swapfile &&
-    mkswap /var/swapfile
+    dd if=/dev/zero of=/mnt/swap bs=1M count=#{swap_size} &&
+    chmod 600 /mnt/swap &&
+    mkswap /mnt/swap
     eof
   end
 
   mount '/dev/null' do  # swap file entry for fstab
     action :enable  # cannot mount; only add to fstab
-    device '/var/swapfile'
+    device '/mnt/swap'
     fstype 'swap'
   end
 
