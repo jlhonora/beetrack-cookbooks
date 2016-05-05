db4c500056db3c482b3645a2595307af47b788d0
jlhonora <jlhonora@ing.puc.cl>
2016-05-05 01:04:30 -0300

    Try to run as original user

--- beetrack_rails/recipes/bundle_install.rb
+++ beetrack_rails/recipes/bundle_install.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |application, deploy|
   execute "bundle install" do
     cwd  release_path
     user username
-    command "cd #{release_path} && bundle install --path /home/#{username}/.bundler/#{application}"
+    command "sudo su - #{username} -c 'cd #{release_path} && bundle install --path /home/#{username}/.bundler/#{application}'"
     returns [0,1]
   end
 end
