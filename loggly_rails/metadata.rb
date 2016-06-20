8efa4512a649fac67ac5140a8db1cfccfe9db0a8
John Owen <johnowenatala@gmail.com>
2016-06-20 17:32:01 -0400

    New recipe: loggly setup for rails using rsyslog

---
+++ loggly_rails/metadata.rb
@@ -0,0 +1,11 @@
+name              'loggly_rails'
+maintainer        'John Owen'
+maintainer_email  'john.owen@beetrack.com'
+license           'Apache 2.0'
+description       'Installs/Configures Loggly for Rails'
+long_description  'Installs/Configures Loggly for Rails as Loggly documentation says'
+version           '1.0.0'
+
+supports          'ubuntu', '>= 12.04'
+
+depends           'rsyslog', '> 1.3'
